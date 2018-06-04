5c302ba4c6b77b0b072ceec27f65c67fd736a1ef
Beibei Wang <beibeiwang.beibei@gmail.com>
2018-06-03 21:18:40 -0400

    remove canvas

--- app.js
+++
@@ -1,6 +0,0 @@
-var canvas  = document.getElementById("canvas_elem");
-if (canvas.getContext) {
-    var canvas_context = canvas.getContext('2d');
-} else {
-    console.log('Sorry your browser does not support canvas');
-}
